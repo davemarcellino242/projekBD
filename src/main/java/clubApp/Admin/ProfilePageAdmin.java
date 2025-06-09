c3d6aee1964153a27cb4ec9043d879e5f8eda5df
davemarcellino242 <davealiyan123@gmail.com>
2025-06-09 11:33:47 +0700

    init commit

--- src/main/java/clubApp/Admin/ProfilePageAdmin.java
+++ src/main/java/clubApp/Admin/ProfilePageAdmin.java
@@ -1,6 +1,9 @@
 package clubApp.Admin;
 
+import DAO.MahasiswaDAO;
+import Model.mahasiswa;
 import currentUser.SessionManager;
+import db.DBConnector;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,12 +11,49 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ProfilePageAdmin {
+
+    @FXML
+    private TextField nrpField, namaField, emailField, tanggalLahirField, fakultasField, prodiField, programField;
+
+    @FXML
+    private Connection conn;
+    @FXML
+    public void initialize(){
+        try{
+            conn = DBConnector.connect();
+            MahasiswaDAO dao = new MahasiswaDAO();
+
+            String currentUserNrp = SessionManager.getCurrentUser().getNrp();
+            mahasiswa mhs = dao.getMahasiswaByNrp(currentUserNrp);
+
+            if(mhs != null){
+                nrpField.setText(mhs.getNrp());
+                nrpField.setEditable(false);
+                namaField.setText(mhs.getNama());
+                namaField.setEditable(false);
+                emailField.setText(mhs.getEmail());
+                emailField.setEditable(false);
+                tanggalLahirField.setText(mhs.getTanggalLahir().toString());
+                tanggalLahirField.setEditable(false);
+                fakultasField.setText(mhs.getNamaFakultas());
+                fakultasField.setEditable(false);
+                prodiField.setText(mhs.getNamaProgramStudi());
+                prodiField.setEditable(false);
+                programField.setText(mhs.getNamaProgram());
+                programField.setEditable(false);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @FXML
     public void profilePageAdmin(ActionEvent event) throws IOException {
